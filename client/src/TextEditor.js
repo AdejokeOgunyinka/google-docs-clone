ccccb3776f7f033c03eb9ff192186718da88b0f8
Adejoke <ogunyinkajoke@gmail.com>
2021-08-07 14:36:55 +0100

    Full implementation of google-docs-clone

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -3,7 +3,9 @@ import { useCallback, useEffect, useState } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 import { io } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
 
+const SAVE_INTERVAL_MS = 2000;
 
 const TOOLBAR_OPTIONS = [
     [{ header: [ 1, 2, 3, 4, 5, 6, false ] }],
@@ -15,7 +17,7 @@ const TOOLBAR_OPTIONS = [
     [{ align: [] }],
     [ "image", "blockquote", "code-block" ],
     [ "clean" ]
-]
+];
 
 const TextEditor = () => {
     // const wrapperRef = useRef();
@@ -34,6 +36,7 @@ const TextEditor = () => {
 
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
+    const {id: documentId} = useParams();
 
     useEffect(() => {
         const s = io("http://localhost:3001");
@@ -45,11 +48,51 @@ const TextEditor = () => {
     }, []);
 
     useEffect(() => {
-        if (socket == null || quill == null) return
+        if (socket == null || quill == null ) return
+        socket.once("load-document", document => {
+            quill.setContents(document);
+            quill.enable();
+        })
+        socket.emit("get-document", documentId);
+
+
+        return () => {
+            
+        }
+    }, [socket, quill, documentId]);
+
+    useEffect(() => {
+        if (socket == null || quill == null ) return
+        const interval = setInterval(() => {
+            socket.emit("save-document", quill.getContents())
+        }, SAVE_INTERVAL_MS);
+
+        return () => {
+            clearInterval(interval);
+        }
         
+    }, [socket, quill])
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        const handler = (delta) => {
+            quill.updateContents(delta);
+        }
+
+        socket.on("receive-changes", handler);
+        return () => {
+            socket.off("receive-changes", handler);
+        }
+
+    }, [socket, quill]);
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
         const handler = (delta, oldDelta, source) => {
             if (source !== "user") return ;
-            source.emit("send-changes", delta)
+            socket.emit("send-changes", delta)
         }
 
         quill.on("text-change", handler);
@@ -66,6 +109,10 @@ const TextEditor = () => {
         const editor = document.createElement("div");
         wrapper.append(editor);
         const q = new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS} });
+
+        q.disable();
+        q.setText("Loading...");
+
         setQuill(q);
     
     }, [])
--- server/server.js
+++ server/server.js
@@ -1,10 +1,41 @@
- const io = require("socket.io")(3001, {
+const mongoose = require("mongoose");
+const Document = require("./document");
+
+mongoose.connect("mongodb+srv://Adejoke:_i5$7CW$rW2-$E$@cluster0.2f8pg.mongodb.net/google-docs-clone?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+
+const io = require("socket.io")(3001, {
     cors: {
         origin: "http://localhost:3000",
         methods: [ "GET", "POST" ]
     }
  });
 
- io.on("connection", socket => {
-    console.log("connected");
- });
+const defaultValue = "";
+
+io.on("connection", socket => {
+    socket.on("get-document", async documentId => {
+        const document = await findOrCreateDocument(documentId);
+        socket.join(documentId);
+        socket.emit("load-document", document.data);
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta);
+        });
+        
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
+    })
+});
+
+const findOrCreateDocument = async (id) => {
+    if(id == null ) return;
+    const document = await Document.findById(id);
+    if (document) return document;
+    return Document.create({_id: id, data: defaultValue});
+};
