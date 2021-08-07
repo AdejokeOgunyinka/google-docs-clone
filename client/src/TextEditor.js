c43703ed0c2c4e1b5d53f0a7d3c28e81273e5a99
Adejoke <ogunyinkajoke@gmail.com>
2021-08-07 11:56:32 +0100

    Styled the Frontend view of the google docs clone

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -3,6 +3,19 @@ import { useCallback } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [ 1, 2, 3, 4, 5, 6, false ] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [ "bold", "italic", "underline" ],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    [ "image", "blockquote", "code-block" ],
+    [ "clean" ]
+]
+
 const TextEditor = () => {
     // const wrapperRef = useRef();
     
@@ -19,17 +32,17 @@ const TextEditor = () => {
     // }, [])
 
     const wrapperRef = useCallback((wrapper) => {
-        
+
         if (wrapper === null) return
         wrapper.innerHTML = "";
         const editor = document.createElement("div");
         wrapper.append(editor);
-        new Quill(editor, { theme: "snow" });
+        new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS} });
     
     }, [])
 
     return (
-        <div id="container" ref={wrapperRef}>
+        <div className="container" ref={wrapperRef}>
 
         </div>
     )
